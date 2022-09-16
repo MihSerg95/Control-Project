c2bf145a600b9d7b724e9e531b248450b50e2bf8
Mikhail <mikhailpanov1995@example.com>
2022-09-16 16:36:24 +0700

    Создали первую функцию

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] text1 = new string[4] {"hello", "2", "world", ":-)"};
 string[] text2 = new string[text1.Length];
+
+void SecondText(string[] text1, string[] text2)
+{
+    int count = 0;
+    for (int i = 0; i < text1.Length; i++)
+    {
+        if(text1[i].Length <= 3)
+        {
+            text2[count] = text1[i];
+            count++;
+        }
+    }
+}
